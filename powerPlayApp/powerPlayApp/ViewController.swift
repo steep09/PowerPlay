061b7419b9573f522e0374239837fff0d4300695
Stephenson Ang <stiph@Stephensons-MacBook-Pro.local>
2019-05-15 16:46:56 +0800

    initial comiit. Models created

---
+++ powerPlayApp/powerPlayApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  powerPlayApp
+//
+//  Created by Stephenson Ang on 15/05/2019.
+//  Copyright © 2019 Stephenson Ang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
